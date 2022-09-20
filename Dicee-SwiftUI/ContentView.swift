88a8e59b4407a9a70b34a7967dcad3220059a969
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-09-20 22:17:25 +0300

    created UI

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack{
+                    DiceView(n: 1)
+                    DiceView(n: 2)
+                }.padding(.horizontal)
+                Spacer()
+                Button(action: { })
+                    {
+                        Text("Roll")
+                            .font(.system(size: 50))
+                            .fontWeight(.heavy)
+                            .foregroundColor(.white)
+                            .padding(.horizontal)
+                            
+                            
+                    }
+                    .background(.red)
+                    .cornerRadius(15)
+                Spacer()
+              
+                
+
+            }
         }
-        .padding()
+    }
+}
+
+struct DiceView: View {
+    var n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
 
@@ -24,3 +57,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+ 
